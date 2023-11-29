aeead206864c7c9381501c1591861fa33fc484e9
Kulshresth jangid <kulshresthjangid@gmail.com>
2023-11-30 01:01:11 +0530

    FindFloorOfNumber.java

---
+++ FindFloorOfNumber.java
@@ -0,0 +1,27 @@
+public class FindFloorOfNumber {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 5, 9, 14, 16, 18 };
+        int[] arr2 = { 2, 8, 12, 14, 18, 24, 30 };
+        System.out.println(findFloorOfNumber(arr, 15));
+        System.out.println(findFloorOfNumber(arr2, 15));
+        System.out.println(findFloorOfNumber(arr2, 3));
+        System.out.println(findFloorOfNumber(arr2, 25));
+        System.out.println(findFloorOfNumber(arr2, 10));
+    }
+
+    public static int findFloorOfNumber(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return arr[mid];
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            }
+        }
+        return arr[end];
+    }
+}
