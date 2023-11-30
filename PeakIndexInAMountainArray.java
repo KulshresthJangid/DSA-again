35c9dbab2031c76343fffaadd9cfd85810ed1631
Kulshresth jangid <kulshresthjangid@gmail.com>
2023-12-01 02:18:55 +0530

    PeakIndexInAMountainArray

--- PeakIndexInAMountainArray.java
+++ PeakIndexInAMountainArray.java
@@ -12,11 +12,26 @@ public class PeakIndexInAMountainArray {
             if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
                 return mid;
             } else if (arr[mid] < arr[mid - 1] && arr[mid] > arr[mid + 1]) {
-                end = mid;
+                end = mid - 1;
             } else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
-                start = mid;
+                start = mid + 1;
             }
         }
         return -1;
+    }
+
+    public static int kunalSolution(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+
     }
 }
