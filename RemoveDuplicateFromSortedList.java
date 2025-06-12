cb8cd05a9c285563724902315e5b52faf2783787
kulshresthjangid <jangidkulshresth@gmail.com>
2025-06-13 04:55:31 +0530

    Fixing problenm 83

--- RemoveDuplicateFromSortedList.java
+++ RemoveDuplicateFromSortedList.java
@@ -7,18 +7,28 @@ public class RemoveDuplicateFromSortedList {
         ListNode next;
         ListNode() {}
         ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) {this.val; this.next = next;}
+        ListNode(int val, ListNode next) {this.val = val; this.next = next;}
     }
 
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode nextNode = head.next;
-        while (head.val == nextNode.val && head.next != null) {
-            if(head.val == nextNode.val) {
-                 
-            }
-        }
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode prev = dummyNode;
+        ListNode cur = head;
 
-        return head;
+        while (cur != null && cur.next != null) {
+            if(cur.val == cur.next.val) {
+                while (cur.next != null && cur.val == cur.next.val) {
+                    cur= cur.next;
+                }
+                prev.next = cur.next;
+            } else {
+                prev = cur.next;
+            }
+            cur = cur.next;
+        }
+        
+        return dummyNode.next;
     }
     
 }
---
+++ RemoveDuplicatesFromSortedList82.java
@@ -0,0 +1,87 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class RemoveDuplicatesFromSortedList82 {
+
+    public static void main(String[] args) {
+
+        RemoveDuplicatesFromSortedList82.ListNode linkedList = new RemoveDuplicatesFromSortedList82.ListNode(
+                1,
+                new RemoveDuplicatesFromSortedList82.ListNode(1,
+                        new RemoveDuplicatesFromSortedList82.ListNode(2)));
+        // LinkedList<Integer> list = new LinkedList<>();
+        // int i = 0;
+        // RemoveDuplicatesFromSortedListLinkedList.ListNode ansLinkedList = new ListNode();
+        // getUniqueNumber(linkedList, list);
+        // for (int k = 0; k < list.size(); k++) {
+        //     ansLinkedList.val = list.get(k);
+        //     if(++k<=list.size()-1) {
+        //         ansLinkedList.next = new ListNode(list.get(++k));
+        //     }
+        // }
+
+        ListNode current = linkedList;
+
+        // while (current != null && current.next != null) {
+        //     if (current.val == current.next.val) {
+        //         current.next = current.next.next;
+        //     } else {
+        //         current = current.next;
+        //     }
+        // }
+        deleteDuplicateRec(current);
+        System.out.println("got here no call stack");
+
+
+    }
+
+    public static void getUniqueNumber(ListNode list, List<Integer> ansList) {
+        if (Objects.isNull(list.next)) {
+            if (!ansList.contains(list.val)) {
+                ansList.add(list.val);
+            }
+            return;
+        } else {
+            if (!ansList.contains(list.val)) {
+                ansList.add(list.val);
+            }
+            list = list.next;
+            getUniqueNumber(list, ansList);
+        }
+    }
+
+    public static void deleteDuplicateRec(ListNode head) {
+        if(Objects.isNull(head.next)) {
+            return;
+        } else {
+            if(head.val == head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        deleteDuplicateRec(head);
+    }
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+}
