2e943d92bbdc4f9371384c901353bc648054d6a9
Kulshresth Jangid <jangidkulshresth@gmail.com>
2026-03-27 02:12:17 +0530

    feat: changing the whoel structure of the repo as this repo will be the base of learning of all implementatoins

--- DSA/NumberOfSubSequencesThatSatisfiesGivenSumCondition.java
+++ DSA/NumberOfSubSequencesThatSatisfiesGivenSumCondition.java
@@ -5,16 +5,16 @@ import java.util.Arrays;
 public class NumberOfSubSequencesThatSatisfiesGivenSumCondition {
     public static void main(String[] args) {
         // int[] nums = {3,5,6,7};
-        int[] nums = {2,3,3,4,6,7};
+        int[] nums = {14,4,6,6,20,8,5,6,8,12,6,10,14,9,17,16,9,7,14,11,14,15,13,11,10,18,13,17,17,14,17,7,9,5,10,13,8,5,18,20,7,5,5,15,19,14};
         Arrays.sort(nums);
-        int target = 12;
+        int target = 22;
         int left = 0, right = nums.length -1;
         int result = 0;
         while(left<=right) {
             int sum = nums[left] + nums[right];
 
             if(sum <= target) {
-                result = (int) (result + Math.pow(2,right - left));
+                result += (int) (Math.pow(2,right - left));
                 left++;
             } else {
                 right--;
